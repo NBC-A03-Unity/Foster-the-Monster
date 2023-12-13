3ecfe3624ee9eaaf0399d1780708b2753e4e6474
문현우 <84183932+Munch310@users.noreply.github.com>
2023-12-13 16:35:03 +0900

    enhancement : "File Location"
    - 파일 위치 조정
    - 스크립터블 오브젝트 파일-> 세부 폴더 이동

--- ScriptableObjects/EventFuncSO/Script/BossRunEventSO.cs
+++
@@ -1,9 +0,0 @@
-using System;
-public class BossRunEventSO : EventFuncSO
-{
-
-    public override Action Apply(Action complete, int value = 0)
-    {
-        return () => EventManager.Instance.WaringEvent("BossRunGameOver");
-    }
-}
--- ScriptableObjects/EventFuncSO/Script/EventFuncSO.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-public abstract class EventFuncSO : ScriptableObject
-
-{
-    public abstract Action Apply(Action complete, int value = 0);
-
-}
